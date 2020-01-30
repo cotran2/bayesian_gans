fd86e39baa915d97525a97d7fa4a163138679bdc
cotran2 <cotran2@utexas.edu>
2020-01-30 14:38:36 -0600

    some small changes

--- codes/distributions/distributions.py
+++ codes/distributions/distributions.py
@@ -1,75 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib import cm
-from mpl_toolkits.mplot3d import Axes3D
-
-
-
-def multivariate_gaussian(pos, mu, Sigma):
-    """Return the multivariate Gaussian distribution on array pos.
-
-    pos is an array constructed by packing the meshed arrays of variables
-    x_1, x_2, x_3, ..., x_k into its _last_ dimension.
-
-    """
-
-    n = mu.shape[0]
-    Sigma_det = np.linalg.det(Sigma)
-    Sigma_inv = np.linalg.inv(Sigma)
-    N = np.sqrt((2*np.pi)**n * Sigma_det)
-    # This einsum call calculates (x-mu)T.Sigma-1.(x-mu) in a vectorized
-    # way across all the input variables.
-    fac = np.einsum('...k,kl,...l->...', pos-mu, Sigma_inv, pos-mu)
-
-    return np.exp(-fac / 2) / N
-def test_2d_visualization():
-    size = 100
-    sigma_x = 6.
-    sigma_y = 2.
-
-    x = np.linspace(-10, 10, size)
-    y = np.linspace(-10, 10, size)
-
-    x, y = np.meshgrid(x, y)
-    z = (1 / (2 * np.pi * sigma_x * sigma_y) * np.exp(-(x ** 2 / (2 * sigma_x ** 2)
-                                                        + y ** 2 / (2 * sigma_y ** 2))))
-
-    plt.contourf(x, y, z, cmap='Blues')
-    plt.colorbar()
-    plt.show()
-def test_3d_visualization():
-    # Our 2-dimensional distribution will be over variables X and Y
-    N = 60
-    X = np.linspace(-3, 3, N)
-    Y = np.linspace(-3, 4, N)
-    X, Y = np.meshgrid(X, Y)
-
-    # Mean vector and covariance matrix
-    mu = np.array([0., 1.])
-    Sigma = np.array([[1., -0.5], [-0.5, 1.5]])
-
-    # Pack X and Y into a single 3-dimensional array
-    pos = np.empty(X.shape + (2,))
-    pos[:, :, 0] = X
-    pos[:, :, 1] = Y
-    # The distribution on the variables X, Y packed into pos.
-    Z = multivariate_gaussian(pos, mu, Sigma)
-
-    # Create a surface plot and projected filled contour plot under it.
-    fig = plt.figure()
-    ax = fig.gca(projection='3d')
-    ax.plot_surface(X, Y, Z, rstride=3, cstride=3, linewidth=1, antialiased=True,
-                    cmap=cm.viridis)
-
-    cset = ax.contourf(X, Y, Z, zdir='z', offset=-0.15, cmap=cm.viridis)
-
-    # Adjust the limits, ticks and view angle
-    ax.set_zlim(-0.15, 0.2)
-    ax.set_zticks(np.linspace(0, 0.2, 5))
-    ax.view_init(27, -21)
-
-    plt.show()
-
 
 class Distribution():
     """
@@ -148,6 +78,8 @@ class Distribution():
         samples = np.array(samples[burnin_size:])
         samples = np.reshape(samples, [samples.shape[0], 2])
         return samples
+
+
 if __name__ == "__main__":
     d = Distribution()
     d.visualize(options = 'sampling_1')
