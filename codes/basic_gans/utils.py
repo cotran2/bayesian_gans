57bb9a5c1117ba4ad0ad849350453f7a8cf6d586
cotran2 <cotran2@utexas.edu>
2020-02-15 00:25:21 -0600

    fix bugs in distribution class

--- codes/basic_gans/utils.py
+++ codes/basic_gans/utils.py
@@ -2,7 +2,41 @@ import tensorflow as tf
 from tensorflow.keras import datasets
 import numpy as np
 from tqdm import tqdm
+import sys
+import os
+sys.path.append(os.path.abspath('../../'))
 from codes.distributions import distributions as dist
+def distribution_1(z, alpha = 1, ):
+    sigma_1 = 0.1
+    sigma_2 = 0.1
+    mu_1 = 1
+    mu_2 = 1
+    z = np.reshape(z, [z.shape[0], 2])
+    x_1, x_2 = z[:, 0], z[:, 1]
+    norm = (1 / (2 * np.pi * sigma_1 * sigma_2))
+    exp1 = (np.sqrt(x_1 ** 2 + x_2 ** 2)- mu_1) ** 2 / (2 * sigma_1 ** 2)
+    exp2 = (x_2 - mu_2) ** 2 / (2 * sigma_2 ** 2)
+    z = norm*(np.exp(-exp1-exp2))
+    return x_1,x_2,z
+
+def distribution_2( z, alpha = 1):
+    z = np.reshape(z, [z.shape[0], 2])
+    x_1, x_2 = z[:, 0], z[:, 1]
+    sigma_1 = 1/10
+    sigma_2 = np.sqrt(10)
+    z = (1 / (2 * np.pi * sigma_1 * sigma_2))*np.exp(-(x_2 - x_1**2) ** 2 /(2*sigma_1**2) -
+                                                      (x_1 - 1) ** 2 / (2* sigma_2 ** 2))
+
+    return x_1, x_2, z
+
+def gaussian(z, alpha = 1):
+    z = np.reshape(z, [z.shape[0], 2])
+    x_1, x_2 = z[:, 0], z[:, 1]
+    sigma_1 = 1
+    sigma_2 = 1
+    z = (1 / (2 * np.pi * sigma_1 * sigma_2)) * np.exp(-(x_1 ) ** 2 / (2 * sigma_1 ** 2) -
+                                                       (x_2) ** 2 / (2 * sigma_2 ** 2))
+
 
 def get_data_mnist():
     """
@@ -30,12 +64,14 @@ def get_data_distribution(params):
     """
     x_train,y_train,x_test,y_test = [],[],[],[]
     d = dist.Distribution()
-    for _ in tqdm(range(params.n_samples)):
+    for _ in range(params.n_samples):
         x_train.append(np.random.multivariate_normal(mean=[0,0],cov=np.eye(2),size=params.sampling_size))
         if params.dataset == "1":
-            y_train.append(d.metropolis_hastings(d.distribution_1,sampling_size= params.sampling_size))
+            func = distribution_1
+
         elif params.dataset == "2":
-            y_train.append(d.metropolis_hastings(d.distribution_2,sampling_size= params.sampling_size))
+            func = distribution_2
+        y_train.append(d.metropolis_hastings(func, sampling_size=params.sampling_size))
     x_train = tf.convert_to_tensor(np.array(x_train), np.float32)
     y_train = tf.convert_to_tensor(np.array(y_train), np.float32)
     return x_train,y_train,x_test,y_test
